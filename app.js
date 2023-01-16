e6a2b593490882bb6263e008191bfaa0da5a816c
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-16 00:55:08 -0600

    updating to my app + moving HTML/CSS elsewhere

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
+require('dotenv').config()
 const express = require("express");
-const app = express();
+const path = require('path');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const register = require("./controllers/register");
+const signIn = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 const port = process.env.PORT || 3001;
+const DB_HOST = process.env.ztm_sbdb_host;
+const DB_PORT = process.env.ztm_sbdb_port;
+const DB_USER = process.env.ztm_sbdb_user;
+const DB_PW = process.env.ztm_sbdb_pw;
+const DB_NAME = process.env.ztm_sbdb;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+      host : DB_HOST,
+      port : DB_PORT,
+      user: DB_USER,
+      password : DB_PW,
+      database : DB_NAME
+  }
+  //searchPath: ['knex', 'public'],
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
 
+// Setting up the public directory
+app.use(express.static('public'));
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+function getUser(id) {
+  let found = false;
+  db.users.forEach(user => {
+      if (user.id === id) {
+          found = true;
+          return res.status(200).json(user);
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  })
+  if (!found) {
+      res.status(400).json('not found');
+  }
+}
+
+app.get("/", (req, res) => res.status(200).sendFile(path.join(__dirname, '/public/index.html')));
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // dependency injuection;
+// app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
+app.post('/signin', signIn.handleSignIn(db, bcrypt) )
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+app.put('/image', (req, res) => { image.handleIMG(req, res, db) })
+app.post('/imageURL', (req, res) => { image.handleAPICall(req, res) })
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
